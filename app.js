7157bd30a1ec896da73765450b86b5897c3836bd
Andre Batista <andrebatista@Andres-MacBook-Air.local>
2019-04-11 22:22:22 -0400

    intialized firebase, api key and url call, on click modal button functions in progress

--- app.js
+++ app.js
@@ -1,9 +1,54 @@
-var weatherApiKey = "6fc19ba254fc6bfa17075467ace4ee41";
-var weatherURL = "https://api.openweathermap.org/data/2.5/weather?zip=" + "" + ",us" + "&appid=" + weatherApiKey;
+// 1. Create Firebase link
+// 1a. Initialize Firebase
+<script src="https://www.gstatic.com/firebasejs/5.9.4/firebase.js"></script>
 
-$.ajax({
-    url: weatherURL,
-    method: "GET"
-  }).then(function(weatherNow) {
-    console.log(weatherNow)
-  }); 
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCYpBOTJB_qFP0KMAu6q0KU3uH63iT9y2w",
+    authDomain: "git-grub.firebaseapp.com",
+    databaseURL: "https://git-grub.firebaseio.com",
+    projectId: "git-grub",
+    storageBucket: "git-grub.appspot.com",
+    messagingSenderId: "699814957038"
+  };
+
+  firebase.initializeApp(config);
+
+
+// 2. Create initial train data in database (You can do this through the Firebase console online)
+var database = firebase.database();
+
+
+
+// 3. weather api key and call
+var weatherApiKey = "6fc19ba254fc6bfa17075467ace4ee41";
+var userZip;
+var weatherURL = "https://api.openweathermap.org/data/2.5/weather?zip=" + userZip + ",us" + "&appid=" + weatherApiKey;
+
+
+
+
+// ------------------ functions ----------------
+// ------------------ still in progress ------------
+
+// 4. Ajax call function
+// $.ajax({
+//     url: weatherURL,
+//     method: "GET"
+//   }).then(function(weatherNow) {
+
+
+// ----------- method I found on Stack overflow, but havent gotten it to work either ---------
+
+//   $(document).on("click", "btn-primary", function(event){
+//     console.log( "click" ); 
+//   });
+    
+
+// -------------- My method -----------------
+
+    //   $(".btn-primary").on("click", function(event){
+    //      event.preventDefault()
+    //     userZip = $("#saveChangesButton").val().trim()
+    //     console.log("click");
+    // });
