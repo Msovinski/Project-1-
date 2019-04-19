9c08791cec7371c11e98c6c029d83d5d2c241c83
Msovinski <47647831+Msovinski@users.noreply.github.com>
2019-04-18 20:31:09 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,99 +1,206 @@
-// 1. Create Firebase link
-// 1a. Initialize Firebase
 
+  // 1. Create Firebase link
+  // 1a. Initialize Firebase
 
   // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCYpBOTJB_qFP0KMAu6q0KU3uH63iT9y2w",
-    authDomain: "git-grub.firebaseapp.com",
-    databaseURL: "https://git-grub.firebaseio.com",
-    projectId: "git-grub",
-    storageBucket: "git-grub.appspot.com",
-    messagingSenderId: "699814957038"
-  };
+  // var config = {
+  //   apiKey: "AIzaSyCYpBOTJB_qFP0KMAu6q0KU3uH63iT9y2w",
+  //   authDomain: "git-grub.firebaseapp.com",
+  //   databaseURL: "https://git-grub.firebaseio.com",
+  //   projectId: "git-grub",
+  //   storageBucket: "git-grub.appspot.com",
+  //   messagingSenderId: "699814957038"
+  // };
 
-  firebase.initializeApp(config);
+  // firebase.initializeApp(config);
 
+  // 2. Create variables to store user data in database (You can do this through the Firebase console online)
+  // var database = firebase.database();
 
-// 2. Create variables to store user data in database (You can do this through the Firebase console online)
-var database = firebase.database();
+  // 3. weather api key and call
+  // var weatherApiKey = "6fc19ba254fc6bfa17075467ace4ee41";
+  // var weatherURL =
+  //   "https://api.openweathermap.org/data/2.5/weather?zip=" +
+  //   "33330" +
+  //   ",us" +
+  //   "&appid=" +
+  //   weatherApiKey;
+  // console.log(weatherURL);
 
-// 3. weather api key and call
-var weatherApiKey = "6fc19ba254fc6bfa17075467ace4ee41";
-var weatherURL = "https://api.openweathermap.org/data/2.5/weather?zip=" + "33330" + ",us" + "&appid=" + weatherApiKey;
-console.log(weatherURL);
+  // ------------------ functions ----------------
+  // ------------------ still in progress ------------
 
+  //4. Ajax call function
+  // function ajaxCall(search) {
+  //   $.ajax({
+  //     url: weatherURL,
+  //     method: "GET"
+  //   }).then(function(weather) {
+  //     console.log(weather.data[0]);
 
+  //     var weatherResults = weather.data;
+  //     console.log(weatherResults);
+  //   });
+  // }
 
-// ------------------ functions ----------------
-// ------------------ still in progress ------------
+  // event listener for party size that hides or shows options
+  $("#partyConfirm").on("click", function() {
+    var members = $("#party-input")
+      .val()
+      .trim();
 
-//4. Ajax call function
-function ajaxCall(search){
-  $.ajax({
-      url: weatherURL,
-      method: "GET"
-  }).then(function(weather){
-      console.log(weather.data[0]);
-
-      var weatherResults = weather.data;
-      console.log(weatherResults);
+    if (members < 2) {
+      document.getElementById("crave-2-input").style.visibility = "hidden";
+    } else {
+      document.getElementById("crave-2-input").style.visibility = "visible";
+    }
+    if (members >= 3) {
+      document.getElementById("crave-3-input").style.visibility = "visible";
+    } else {
+      document.getElementById("crave-3-input").style.visibility = "hidden";
+    }
+    if (members >= 4) {
+      document.getElementById("crave-4-input").style.visibility = "visible";
+    } else {
+      document.getElementById("crave-4-input").style.visibility = "hidden";
+    }
+    if (members >= 5) {
+      document.getElementById("crave-5-input").style.visibility = "visible";
+    } else {
+      document.getElementById("crave-5-input").style.visibility = "hidden";
+    }
   });
 
-  
-};
- $("#partyConfirm").on("click", function(){
-    var members = $("#party-input").val().trim();
+  // click function/event listener for save changes
+  $("#save").on("click", function(event) {
+    event.preventDefault();
 
-    if(members < 2){
-        document.getElementById("crave-2-input").style.visibility = "hidden";
-    } else {
-        document.getElementById("crave-2-input").style.visibility = "visible"
-    }
-    if(members >= 3){
-        document.getElementById("crave-3-input").style.visibility = "visible";
-    } else {
-        document.getElementById("crave-3-input").style.visibility = "hidden"
-    }
-    if(members >= 4){
-        document.getElementById("crave-4-input").style.visibility = "visible";
-    } else {
-        document.getElementById("crave-4-input").style.visibility = "hidden"
-    }
-    if(members >= 5){
-        document.getElementById("crave-5-input").style.visibility = "visible";
-    } else {
-        document.getElementById("crave-5-input").style.visibility = "hidden"
-    }
+    // obtain zip code and party size values
+    var zipcode = $("#zip-input")
+      .val()
+      .trim();
+    var members = $("#party-input")
+      .val()
+      .trim();
 
-$("#save").on("click", function(event){
-  event.preventDefault()
-  var cravings1 = $("#crave-1-input").val().trim();
-  var cravings2 = $("#crave-2-input").val().trim();
-  var cravings3 = $("#crave-3-input").val().trim();
-  var cravings4 = $("#crave-4-input").val().trim();
-  var cravings5 = $("#crave-5-input").val().trim();
-  var zipcode = $("#zip-input").val().trim();
-  var members = $("#party-input").val().trim();
-// make an object to store the user input
-  var input = {
-
-       crave1: cravings1,
-       crave2: cravings2,
-       crave3: cravings3,
-       crave4: cravings4,
-       crave5: cravings5,
-       location: zipcode,
-      party: members,
-  
-  };
+    // make an object to store the user input
+    var input = {
+      location: zipcode,
+      party: members
+    };
     console.log(input);
-  
-   // $("#partyConfirm").on("click", function(event){
-       
-});
-});
 
+    // variable for winning cuisine selection
+    var bestChoice;
+    // pick the majority selected
 
-//Form button control
+    picks.forEach(element => {
+      cuisine[element].picks++;
 
+      // variable for most selected cuisine, to count it?
+      var maxPick = 0;
+
+      // iterating through the cuisines clicked and selecting the most clicked
+      for (var property in cuisine) {
+        var currentPick = cuisine[property].picks;
+        console.log(currentPick);
+        if (currentPick > maxPick) {
+          bestChoice = property.toLowerCase();
+          maxPick = currentPick;
+        }
+      }
+    });
+
+    console.log("Survey says... " + bestChoice); // console logging the result
+    // put the majority and zipcode in the api call
+    $.ajax({
+      async: true,
+      crossDomain: true,
+      url:
+        "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?categories=" +
+        bestChoice +
+        "&location=" +
+        zipcode,
+      method: "GET",
+      headers: {
+        authorization:
+          "Bearer TguaEbsqVv3hvtSt41pPee4Y5t5Gey8ZsSdbkgvmVjPXoN3xOjbUIH-1rXcwhfVk9DResP7FUTSWoWePZqzLi4WMfP1VbRA0fuxRx-vC9uA_GM6VH4sAidogGJCuXHYx",
+        "cache-control": "no-cache"
+      }
+    }).done(function(response) {
+      console.log(response);
+
+      // looping through the desired results and displaying them in the html by dynamically creating a card using bootstrap classes
+      for (var i = 0; i < response.businesses.length; i++) {
+        var newCard = $("<div>");
+        newCard.addClass("col-sm")
+        .addClass("card");
+        var restImage = response.businesses.image;
+        var image = $("<img>");
+        image.addClass("card-img-top");
+        image.attr("src", restImage);
+        newCard.append(image);
+        var newCardBody = $("<div>");
+        newCardBody.addClass("card-body");
+        var newH5 = $("<h5>");
+        newH5.addClass("card-title");
+        var name = response.businesses.name;
+        newCardBody.append(newH5.text(name));
+        var newUl = $("<ul>");
+        newUl.addClass("list-group list-group-flush");
+        var newLi = $("<li>");
+        newLi.addClass("list-group-item");
+        var distance = response.businesses.distance;
+        var newLi2 = $("<li>");
+        newLi2.addClass("list-group-item");
+        var rating = response.businesses.rating;
+        newLi2.append(rating);
+        var newCardBody2 = $("<div>");
+        newCardBody2.addClass("card-body");
+        var a = $("<a>");
+        a.addClass("card-link");
+        var phone = response.businesses.phone;
+        newCardBody2.append(a.text(phone));
+        var a2 =$("<a>");
+        a2.addClass("card-link");
+        var url = response.businesses.url;
+        a2.append(url);
+        $("#venues").append(newCard);
+      } //for loop
+    
+    }); //response
+  }); //save click
+
+  // // event listener to store button value
+  $(".btnChoice").on("click", function(event) {
+    var buttonValue = $(this).text();
+    console.log(buttonValue);
+    picks.push(buttonValue);
+  });
+
+  // global variable for picking cuisines
+  var picks = ["Italian", "American", "Chinese", "Mexican", "Indian"];
+  // global object of cuisine types
+  var cuisine = {
+    Italian: {
+      picks: 0,
+      name: "Italian"
+    },
+    American: {
+      picks: 0,
+      name: "American"
+    },
+    Chinese: {
+      picks: 0,
+      name: "Chinese"
+    },
+    Mexican: {
+      picks: 0,
+      name: "Mexican"
+    },
+    Indian: {
+      picks: 0,
+      name: "Indian"
+    }
+  };
+  console.log(cuisine);
